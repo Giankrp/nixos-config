5d847a4e57b63440681de1ec7b774fc8d72f37ac
Gian <gian@local>
2026-06-17 00:58:58 +0200

    feat: initial working configuration

---
+++ home.nix
@@ -0,0 +1,90 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "gian";
+  home.homeDirectory = "/home/gian";
+
+  # Match the release version of your system
+  home.stateVersion = "26.05";
+
+  # Add directories to PATH
+  home.sessionPath = [
+    "$HOME/.local/bin"
+    "$HOME/.local/share/pnpm/bin"
+  ];
+
+  # Define environment variables
+  home.sessionVariables = {
+    PNPM_HOME = "$HOME/.local/share/pnpm/bin";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Zsh configuration managed by Home Manager
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    enableCompletion = true;
+
+    shellAliases = {
+      v = "nvim";
+      gs = "git status";
+    };
+
+    initContent = ''
+      eval "$(starship init zsh)"
+    '';
+  };
+
+  # Tmux configuration
+  programs.tmux = {
+    enable = true;
+    shortcut = "space"; # prefix C-Space
+
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.catppuccin
+    ];
+
+    extraConfig = ''
+      # Shell configuration
+      set -g default-shell "/run/current-system/sw/bin/zsh"
+
+      # Mappings
+      bind -n M-H previous-window
+      bind -n M-L next-window 
+
+      # Open in current directory
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
+
+      # Catppuccin theme customization
+      set -g @catppuccin_flavor 'mocha'
+
+      # Status bar configuration
+      set -g @catppuccin_window_left_separator ""
+      set -g @catppuccin_window_right_separator " "
+      set -g @catppuccin_window_middle_separator " █"
+      set -g @catppuccin_window_number_position "right"
+
+      set -g @catppuccin_window_default_fill "number"
+      set -g @catppuccin_window_default_text "#W"
+
+      set -g @catppuccin_window_current_fill "number"
+      set -g @catppuccin_window_current_text "#W"
+
+      set -g @catppuccin_status_modules_right "directory git session"
+      set -g @catppuccin_status_left_separator  " "
+      set -g @catppuccin_status_right_separator ""
+      set -g @catppuccin_status_right_separator_inverse "no"
+      set -g @catppuccin_status_fill "icon"
+      set -g @catppuccin_status_connect_separator "no"
+
+      set -g @catppuccin_directory_text "#{pane_current_path}"
+    '';
+  };
+}
